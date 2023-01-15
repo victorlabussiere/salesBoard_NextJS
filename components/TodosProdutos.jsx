39c38bd5b93b9a41e832c73217663b378f383bf2
Victor Labussiere <victor.labussiere95@gmail.com>
2023-01-15 16:10:47 -0300

    first commit

--- components/TodosProdutos.jsx
+++ components/TodosProdutos.jsx
@@ -1,49 +1,55 @@
 import styles from '../styles/TodosProdutos.module.css'
+import Image from 'next/image'
 
 export default function TodosProdutos() {
     return (
         <div className={styles.todosProdutos}>
-            <header>
+            <div className={styles.header}>
                 <h2>Todos os Produtos</h2>
                 <div className={styles.arrowArea}>
                     <i className='material-icons'>arrow_back</i>
                     <i className='material-icons'>arrow_forward</i>
                 </div>
-            </header>
+            </div>
 
             <table className={styles.tabelaProdutos}>
-                <tr>
-                    <th>IDENTIFICAÇÃO</th>
-                    <th>PREÇO</th>
-                    <th>VENDAS</th>
-                    <th>ESTOQUE</th>
-                    <th></th>
-                </tr>
-                <tr>
-                    <td>
-                        <div className={styles.itemDetails}>
-                            <span className={styles.cardImage}></span>
-                            <p>Kit 10 Un. Adesivo 3m Porta Cartão De Silicone Para Celular</p>
-                            <p>#MLB2063247364</p>
-                        </div>
-                    </td>
+                <thead>
+                    <tr>
+                        <th>IDENTIFICAÇÃO</th>
+                        <th>PREÇO</th>
+                        <th>VENDAS</th>
+                        <th>ESTOQUE</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <Image src='/images/mockImage.webp' width={40} height={40} alt='imagem mockada' className={styles.image} />
+                            <div className={styles.rowData}>
+                                <p>Kit 10 Un. Adesivo 3m Porta Cartão De Silicone Para Celular
+                                    <br /><strong className='boldBlue'>#MLB2063247364</strong>
+                                </p>
+                            </div>
+                        </td>
 
-                    <td>
-                        <p>R$ 31,67</p>
-                    </td>
+                        <td>
+                            <p>R$ 31,67</p>
+                        </td>
 
-                    <td>
-                        <p>Total de 4.750</p>
-                        <p>150 vendas</p>
-                    </td>
+                        <td>
+                            <p><strong>Total de 4.750</strong></p>
+                            <p>150 vendas</p>
+                        </td>
 
-                    <td>
-                        <p>0und.</p>
-                    </td>
-                    <td>
-                        <i className='material-icons'>favorite</i>
-                    </td>
-                </tr>
+                        <td>
+                            <p>0 und.</p>
+                        </td>
+                        <td>
+                            <i className='material-icons'>favorite</i>
+                        </td>
+                    </tr>
+                </tbody>
 
             </table>
 
